bf060a4ae335521139e8489db220ffbe2f27739b
mahmoud-hamdan99 <mahmodxxx.mh@gmail.com>
2024-05-22 20:07:46 +0300

    add controller and repos and service

---
+++ DreemDay/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DreemDay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
